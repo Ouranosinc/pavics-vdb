f5428173e447d4e97fd24b5d333d7aa7707f05ec
travis <logan.travis@ouranos.ca>
2019-12-09 14:17:31 -0500

    * Intitial work testing timeunitsChange="true"

---
+++ test_NcMLs/Test_NcML_timechanging.py
@@ -0,0 +1,38 @@
+import xncml
+import os
+import glob
+import numpy as np
+from distutils.dir_util import copy_tree
+import threddsclient
+import xarray as xr
+test_folder = ['TimeChanging','TimeConstant']
+outpath  = './test_data/ncdata_test_changing_time'
+thredds_path = '/home/travis/boreas/testdata/NcML_tests'
+thredds_path_server = 'https://pavics.ouranos.ca/twitcher/ows/proxy/thredds/catalog/birdhouse/testdata/NcML_tests/catalog.html'
+if not os.path.exists(thredds_path):
+    os.makedirs(thredds_path)
+for t in test_folder:
+    nc = xncml.Dataset('./test_data/NcML_templates/NcML_template.ncml')
+    nc.ncroot['netcdf']['aggregation']['scan']['@location'] = t
+    if t == 'TimeChanging':
+        # Use time units change flag
+        nc.ncroot['netcdf']['aggregation']['@timeUnitsChange'] = "true"
+
+    nc.to_ncml(os.path.join(outpath,f'{t}-agg-NcML.ncml'))
+
+copy_tree('./test_data/ncdata_test_changing_time', thredds_path)
+
+datasets = [d.opendap_url() for d in threddsclient.crawl(thredds_path_server, depth=2) if '.ncml' in d.name]
+# opendap
+d0 = xr.open_dataset(datasets[0])
+d0a  = xr.open_mfdataset(sorted(glob.glob('./test_data/ncdata_test_changing_time/TimeConstant/*.nc')), combine='nested', concat_dim='time')
+#
+d1 = xr.open_dataset(datasets[1])
+ds1= xr.open_mfdataset(sorted(glob.glob('./test_data/ncdata_test_changing_time/TimeChanging/*.nc')), combine='nested', concat_dim='time')
+
+np.testing.assert_array_equal(d0.tasmin,d0a.tasmin)
+np.testing.assert_array_equal(d0.time,d0a.time)
+
+np.testing.assert_array_equal(d0.tasmin,d1.tasmin)
+np.testing.assert_array_equal(d0.time, d1.time)
+
