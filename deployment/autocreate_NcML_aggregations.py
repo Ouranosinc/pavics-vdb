2acc3fc897ebd4407d6f3d94e7d99084515fda80
travis <logan.travis@ouranos.ca>
2020-05-01 14:02:37 -0400

    * Batch create CMIP5 ncml [WIP]

--- deployment/autocreate_NcML_aggregations.py
+++ deployment/autocreate_NcML_aggregations.py
@@ -7,9 +7,7 @@ from collections import OrderedDict
 import xncml
 
 home = os.environ['HOME']
-test = xncml.Dataset(
-    f"{home}/github/github_pavics-vdb/1-Datasets/simulations/cmip5/atmos/day_MPI-ESM-LR_historical.ncml")
-pavics_root = f"{home}/boreas"
+pavics_root = f"{home}/boreas/boreas"
 
 
 def main():
@@ -19,20 +17,16 @@ def main():
             ncml_modify = json.load(f)
         ncml_modify
         ncml_template = f'{home}/github/github_pavics-vdb/tests/test_data/NcML_templates/NcML_template_emptyNetcdf.ncml'
-        ncml = xncml.Dataset(ncml_template)
-        # ncml.ncroot['netcdf']
-        ncml.ncroot['netcdf']['remove'] = ncml_remove_items(ncml_modify['remove'])
-        ncml.ncroot['netcdf']['attribute'] = ncml_add_attributes(ncml_modify['attribute'])
 
-        datasets = ncml_create_datasets(ncml=ncml, config=ncml_modify)
+        datasets = ncml_create_datasets(ncml_template=ncml_template, config=ncml_modify)
         for d in datasets.keys():
             outpath = p.Path(str(dataset.parent).replace('dataset_json_configs', '1-Datasets')).joinpath(
-                dataset.name.replace('config.json', f'{d}.ncml'))
+                f"{ncml_modify['frequency']}_{d}.ncml")
             datasets[d].to_ncml(outpath)
 
 
-def ncml_create_datasets(ncml=None, config=None):
-    if config['ncml_type'] == 'cmip5-multirun':
+def ncml_create_datasets(ncml_template=None, config=None):
+    if config['ncml_type'] == 'cmip5-multirun-single-model':
         ncmls = {}
         for exp in config['experiments']:
             agg_dict = {"@dimName": "realization", "@type": "joinNew", "variableAgg": config['variables']}
@@ -50,19 +44,19 @@ def ncml_create_datasets(ncml=None, config=None):
                 netcdf['aggregation'] = ncml_add_aggregation({"@type": "union"})
                 netcdf['aggregation']['netcdf'] = []
                 for v in config['variables']:
-
-                    netcdf2 = ncml_netcdf_container()
-                    netcdf2['aggregation'] = ncml_add_aggregation(
-                        {'@dimName': 'time', '@type': 'joinExisting', '@recheckEvery': '1 day'})
-                    netcdf2['aggregation']['scan'] = []
-                    rcps = exp.split('+')
-                    for rcp in rcps:
-                        path2 = str(path1.joinpath(run,v)).replace('historical',rcp)
-                        scan = {'@location': path2.replace(pavics_root, 'pavics-data'), '@subdirs': 'false', '@suffix':'.nc'}
-                        netcdf2['aggregation']['scan'].append(ncml_add_scan(scan))
-                        del path2
-                    netcdf['aggregation']['netcdf'].append(netcdf2)
-                    del netcdf2
+                    if p.Path(run).joinpath(v).exists():
+                        netcdf2 = ncml_netcdf_container()
+                        netcdf2['aggregation'] = ncml_add_aggregation(
+                            {'@dimName': 'time', '@type': 'joinExisting', '@recheckEvery': '1 day'})
+                        netcdf2['aggregation']['scan'] = []
+                        rcps = exp.split('+')
+                        for rcp in rcps:
+                            path2 = str(path1.joinpath(run,v)).replace('historical',rcp)
+                            scan = {'@location': path2.replace(pavics_root, 'pavics-data'), '@subdirs': 'false', '@suffix':'.nc'}
+                            netcdf2['aggregation']['scan'].append(ncml_add_scan(scan))
+                            del path2
+                        netcdf['aggregation']['netcdf'].append(netcdf2)
+                        del netcdf2
                 agg['netcdf'].append(netcdf)
             agg
             ncml1 = copy.copy(ncml)
@@ -71,6 +65,68 @@ def ncml_create_datasets(ncml=None, config=None):
 
         return ncmls
 
+    elif config['ncml_type'] == 'cmip5-multirun-batch-model':
+        ncmls = {}
+        for exp in config['experiments']:
+            agg_dict = {"@dimName": "realization", "@type": "joinNew", "variableAgg": config['variables']}
+
+            freq = config['frequency']
+            realm = config['realm']
+
+            location = config['location'].replace('pavics-data', pavics_root)
+            for center in [x for x in p.Path(location).iterdir() if x.is_dir()]:
+                print(center)
+                for model in [x for x in p.Path(center).iterdir() if x.is_dir()]:
+                    agg = ncml_add_aggregation(agg_dict)
+                    print(model)
+                    # add runs
+                    agg['netcdf'] = []
+
+                    path1 = p.Path(model).joinpath('historical', freq, realm)
+                    if path1.exists():
+                        run_flag = False
+                        any_run = False
+                        for run in [x for x in path1.iterdir() if x.is_dir()]:
+
+
+                            var_flag = [p.Path(run).joinpath(v).exists() for v in config['variables']]
+                            if all(var_flag):
+                                run_flag=True
+                                netcdf = ncml_netcdf_container(dict={'@coordValue': run.name})
+                                netcdf['aggregation'] = ncml_add_aggregation({"@type": "union"})
+                                netcdf['aggregation']['netcdf'] = []
+                                # ensure all variables are present
+                                for v in config['variables']:
+
+                                    print(str(p.Path(run).joinpath(v)))
+                                    netcdf2 = ncml_netcdf_container()
+                                    netcdf2['aggregation'] = ncml_add_aggregation(
+                                        {'@dimName': 'time', '@type': 'joinExisting', '@recheckEvery': '1 day'})
+                                    netcdf2['aggregation']['scan'] = []
+                                    rcps = exp.split('+')
+                                    for rcp in rcps:
+                                        path2 = str(path1.joinpath(run,v)).replace('historical',rcp)
+                                        if p.Path(path2).exists():
+                                            scan = {'@location': path2.replace(pavics_root, 'pavics-data'), '@subdirs': 'false', '@suffix':'.nc'}
+                                            netcdf2['aggregation']['scan'].append(ncml_add_scan(scan))
+                                            del path2
+                                        else:
+                                            run_flag = False
+                                    netcdf['aggregation']['netcdf'].append(netcdf2)
+                                    del netcdf2
+                                if run_flag:
+                                    any_run = True
+                                    agg['netcdf'].append(netcdf)
+                        # if at least one run has all variables add to list
+                        if any_run:
+                            ncml1 = xncml.Dataset(ncml_template)
+                            ncml1.ncroot['netcdf']['remove'] = ncml_remove_items(config['remove'])
+                            ncml1.ncroot['netcdf']['attribute'] = ncml_add_attributes(config['attribute'])
+                            ncml1.ncroot['netcdf']['aggregation'] = agg
+                            ncmls[f'{model.name}_{exp}'] = ncml1
+                            del ncml1
+
+        return ncmls
 
 def ncml_add_scan(dict=None):
     d1 = OrderedDict()
