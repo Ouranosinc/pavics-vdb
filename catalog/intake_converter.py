a6a4a2a2a1460fdbe35bd03fceec89d0bfb78477
David Huard <huard.david@ouranos.ca>
2021-01-18 17:55:17 -0500

    first working draft version of intake catalog generator

--- catalog/intake_converter.py
+++ catalog/intake_converter.py
@@ -3,7 +3,7 @@
 # Overview
 
 - Given a THREDDS location, walk datasets and subdirectories
-- For each dataset, request NcML service output -> xml (todo : easy using ds.access_urls["NCML"])
+- For each dataset, request NcML service output -> xml
 - Convert xml into dictionary of attributes (extract_attrs_from_ncml)
 - Configure attributes that should go into catalog (todo : ask Travis)
 - Convert list of dicts to csv table using config (todo)
@@ -14,10 +14,55 @@
 from siphon.catalog import TDSCatalog
 
 # Catalog content
-facets = ()
+spec = {
+    "esmcat_version": "0.1.0",
+    "id": "test",
+    "description": "test catalog",
+    "catalog_file": "pavics_datasets.csv",
+    "attributes": [
+        {"column_name": "project_id"},
+        {"column_name": "institute_id"},
+        {"column_name": "type"},
+        {"column_name": "driving_experiment"},
+        {"column_name": "driving_model"},
+        {"column_name": "processing"},
+        {"column_name": "license_type"}
+    ],
+    "assets":{
+        "column_name": "netcdf",
+        "format": "netcdf"
+    }
+}
 
 # Dictionary of facet names, keyed by the standard name that will appear in catalog
 alternative_facet_names = {}
+
+
+def build_catalog(cat):
+    keys = [att["column_name"] for att in spec["attributes"]]
+
+    header = keys + [spec["assets"]["column_name"]]
+    table = [header]
+    for name, gl, gr, va in walk_extract(cat):
+        row = [gl.get(k, "NA") for k in keys]
+        row.append(gr["services"]["OPENDAP"])
+        table.append(row)
+    return table
+
+
+def write_catalog(table):
+    import csv
+    import json
+
+    # Write spec
+    with open("pavics.json", "w") as f:
+        json.dump(spec, f)
+
+    fn = spec["catalog_file"]
+    with open(fn, "w") as f:
+        w = csv.writer(f)
+        for row in table:
+            w.writerow(row)
 
 
 def walk(cat, depth=1):
@@ -38,6 +83,15 @@ def walk(cat, depth=1):
             yield from walk(child, depth=depth-1)
 
 
+def walk_extract(cat, depth=1):
+    out = {}
+    for name, ds in walk(cat):
+        url = ds.access_urls["NCML"]
+        gl, gr, va = extract_attrs_from_ncml(url)
+        gr["services"] = ds.access_urls
+        yield name, gl, gr, va
+
+
 def extract_attrs_from_ncml(url):
     """Extract attributes from NcML file.
 
@@ -56,11 +110,13 @@ def extract_attrs_from_ncml(url):
     """
     import lxml.etree
     import requests
+    parser = lxml.etree.XMLParser(encoding='UTF-8')
+
     ns = {"ncml": "http://www.unidata.ucar.edu/namespaces/netcdf/ncml-2.2"}
 
-    # Parse XML content
-    xml = requests.get(url).text
-    doc = lxml.etree.parse(xml)
+    # Parse XML content - UTF-8 encoded documents need to be read as bytes
+    xml = requests.get(url).content
+    doc = lxml.etree.fromstring(xml, parser=parser)
     nc = doc.xpath("/ncml:netcdf", namespaces=ns)[0]
 
     # Extract global attributes
