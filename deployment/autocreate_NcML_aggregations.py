ee5b63b36fb13bbaa9741c683f1e20908f24d22f
tlogan2000 <logan.travis@ouranos.ca>
2024-10-18 14:57:50 -0400

    update_ncmls

--- deployment/autocreate_NcML_aggregations.py
+++ deployment/autocreate_NcML_aggregations.py
@@ -14,7 +14,7 @@ pavics_root = f"{home}/pavics/datasets"
 def main():
     overwrite_to_tmp = True
     rootdir = p.Path(__file__).parent.parent
-    dataset_configs = rootdir.joinpath("dataset_json_configs").rglob('day_CanDCS-M6_config.json')
+    dataset_configs = rootdir.joinpath("dataset_json_configs").rglob('*CRCM5-CMIP6*.json')
     for dataset in dataset_configs:
         with open(dataset, 'r') as f:
             ncml_modify = json.load(f)
